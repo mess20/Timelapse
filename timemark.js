81e141f91f351348eafe90e3b786c2ebfcaa49b8
Restrechinamynd <87271041+Restrechinamynd@users.noreply.github.com>
2021-09-13 00:07:32 +0800

    Update timemark.js

--- timemark.js
+++ timemark.js
@@ -1,4 +1,4 @@
-window.onload=function timemark(){
+
 var t = document.getElementById("article").innerHTML;
 document.getElementById("article").innerHTML=t
 //大标题
@@ -27,4 +27,3 @@ document.getElementById("article").innerHTML=t
 .replace(new RegExp('」' , "g"), '<note>')
 .replace(new RegExp('（' , "g"), '')
 .replace(new RegExp('）' , "g"), '</note></dfn>')
-}
