7311423d69e2bf4fd434ad34daef3551a78bd898
Restrechinamynd <87271041+Restrechinamynd@users.noreply.github.com>
2021-09-24 00:25:11 +0800

    Create timemark.js

---
+++ timemark.js
@@ -0,0 +1,21 @@
+var t = document.getElementById("article").innerHTML;
+document.getElementById("article").innerHTML=t
+//分割线
+.replace(new RegExp('---' , "g"), '<hr>')
+//强调
+.replace(new RegExp('【' , "g"), '<l>')
+.replace(new RegExp('】' , "g"), '</l>')
+//特殊
+.replace(new RegExp('（' , "g"), '<n>')
+.replace(new RegExp('）' , "g"), '</n>')
+//dfn
+.replace(new RegExp('「' , "g"), '<dfn>')
+.replace(new RegExp('」' , "g"), '<note>')
+.replace(new RegExp('｛' , "g"), '')
+.replace(new RegExp('｝' , "g"), '</note></dfn>')
+//文献
+.replace(new RegExp('〔' , "g"), '<dfn class="reference-index"><span>')
+.replace(new RegExp('〕' , "g"),'</span></dfn>')
+
+var t = document.getElementById("article").innerHTML;
+document.getElementById("article").innerHTML=marked(t);
