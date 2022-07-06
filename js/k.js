7aa3295c329b8ec42943569c76e10a736e770b34
Restrechinamynd <87271041+Restrechinamynd@users.noreply.github.com>
2022-07-06 18:28:08 +0800

    Update and rename k.js to js/k.js

--- js/k.js
+++ js/k.js
@@ -1,7 +1,6 @@
 var main = document.getElementById("article");
 var str = main.innerHTML;
 
-
 function keyword1() {
 document.getElementById("keyword-ul").style.display="block";
 };
@@ -12,7 +11,6 @@ var C = prompt("输入关键词","");
 document.getElementById("searched-button").style.display="block";
 var D = "<k>" + C + "</k>";
 document.getElementById("article").innerHTML= str.replace(new RegExp(C , "g"), D);
-
 document.getElementById("searched-button").addEventListener('click',function searched() {
 main.innerHTML= str.replace(new RegExp(D , "g"), C);
 document.getElementById("searched-button").style.display="none";
@@ -41,7 +39,6 @@ document.getElementById("searched-button").style.display="none";})
 function key3() {
 main.innerHTML= str.replace(new RegExp("容", "g"), "3");
 document.getElementById("searched-button").style.display="block";
-
 document.getElementById("searched-button").addEventListener('click',function f3() {
 main.innerHTML= str.replace(new RegExp("3" , "g"), "容");
 document.getElementById("searched-button").style.display="none";})
